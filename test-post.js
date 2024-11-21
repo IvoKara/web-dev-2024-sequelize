2d74f4fadfa17ffeee712fb2863e20f5bed5249c
IvoKara <ivo.i.karaneshev@gmail.com>
2024-11-21 09:04:06 +0200

    test: create a test POST reqest to subjects

--- test-post.js
+++ test-post.js
@@ -1,8 +1,23 @@
 const axios = require('axios');
 
+
+async function testPostSubjectRequest() {
+
+  const subjects = ["Math", "Science", "Arts", "History"]
+  try {
+    for (let i = 0; i < subjects.length; i++) {
+      const response = await axios.post('http://localhost:3100/subject', {
+        name: subjects[i]
+      });
+      console.log('Response data:', response.data);
+    }
+  } catch (error) {
+    console.error('Error:', error.response ? error.response.data : error.message);
+  }
+}
 async function testPostUniversityRequest() {
   try {
-    const response = await axios.post('http://localhost:3000/university', {
+    const response = await axios.post('http://localhost:3100/university', {
       name: 'Sofia University',
       town: 'Sofia'
     });
@@ -26,6 +41,7 @@ async function testPostUserRequest() {
   }
 }
 (async () => {
+  await testPostSubjectRequest();
   await testPostUniversityRequest();
-  await testPostUserRequest()
+  await testPostUserRequest();
 })()
